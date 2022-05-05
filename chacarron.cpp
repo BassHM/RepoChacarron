9a22c7ca9675658e09042bea68817c5d665cf3db
unknown <jose13.maciasm@gmail.com>
2022-05-05 10:06:46 -0500

    Este esta mejor

--- chacarron.cpp
+++ chacarron.cpp
@@ -2,4 +2,18 @@
 using namespace std;
 int main(){
     cout<<"Hola mundito chikititio chikitito";
+
+    int Pat;
+    int Sebas;
+
+    Pat = 0;
+    Sebas = 1;
+
+    int n;
+    cin>>n;
+    if (n == Pat){
+        cout<<"Eres Pat. Momento truste"<<endl;
+    } else {
+        cout<<"Eres Bass. Chacarron"<<endl;
+    }
 }
